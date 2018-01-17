ed082f41ebdae6dad35221b53ab9266b504794cc
Greg <greg7schmitt1990@gmail.com>
2018-01-17 14:14:27 -0600

    finished first 3 Spring exercises

---
+++ src/main/java/com/codeup/springbootblog/SpringbootBlogApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.springbootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class SpringbootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogApplication.class, args);
+	}
+
+}
